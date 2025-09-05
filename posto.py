a1659006adfd92270396c054feb932911a0db908
bryanS6 <129802677+bryanS6@users.noreply.github.com>
2025-09-05 16:48:40 -0300

    Add files via upload

--- posto.py
+++ posto.py
@@ -1,12 +1,12 @@
 #pedir ao usuario a quantidade de km's percorridos e o consumo de gasolina
 
-gas = float(input("Forneça o consumo de gasolina em ltros"))
+gas = float(input("Forneça o consumo de gasolina em litros"))
 km = float(input("Forneça os km's percorridos"))
 
 gasolina = km / gas
 
 etanol = gasolina * 0.7
 
-print(f"gasolina {gasolina}")
+print(f"gasolina {gasolina}""km/L")
 
-print(f"etanol {etanol}")
+print(f"etanol {etanol}""km/L")
